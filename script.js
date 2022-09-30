cf5a250e7159db43eca8751bd3eb2b7b6de81698
Alexis Herrera <lexibeli@Alexiss-iMac.local>
2022-09-29 22:41:20 -0400

    final

--- script.js
+++ script.js
@@ -1,32 +1,6 @@
 const url = "https://digimon-api.vercel.app/api/digimon";
 
 
-
-
-//Rauls AXIOS
-// function displayUI(arr) {
-//   arr.forEach((person) => {
-//     let htmlTemplate = `
-//       <div class="card">
-//         <h1>${person.name}</h1>
-//         <p>Birth Year: ${person.birth_year}</p>
-//         <p>Height (cm): ${person.height}</p>
-//         <p>Mass (kg): ${person.mass}</p>
-//         <p>Number of films: ${person.films.length}</p>
-//       </div>
-//     `;
-
-//     starContainer.insertAdjacentHTML("afterend", htmlTemplate);
-//   });
-// }
-
-// async function fetchStarWarsPeople() {
-//   let response = await axios(url);
-//   console.log(response);
-// }
-
-//  fetchStarWarsPeople();
-
 let containerDigimon = document.querySelector('.digimon-container')
 let buttonForward = document.querySelector('.forward')
 let buttonBack = document.querySelector('.back')
@@ -36,42 +10,41 @@ fetch(url)
     return response.json();
   })
   .then((digimon) => {
-    
+
     digimon.forEach(digimon => {
       let htmlTemplate = `<li class=character>${digimon.name}<img src='${digimon.img}'> ${digimon.level} <li> `;
       gallery.insertAdjacentHTML('beforeend', htmlTemplate)
-      }
-    
+    }
+
     );
-    
+
   }
-  
-)
+
+  )
   .catch((error) => {
     console.error("Something went wrong...", error);
   });
 
 
 
+let slideIndex = 1;
+showDivs(slideIndex);
 
-    var slideIndex = 1;
-    showDivs(slideIndex);
-    
-    function plusDivs(n) {
-      showDivs(slideIndex += n);
-    }
-    
-    function showDivs(n) {
-      var i;
-      var x = document.getElementsByClassName("character");
-      if (n > x.length) {slideIndex = 1}
-      if (n < 1) {slideIndex = x.length} ;
-      for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-        x[slideIndex - 1].style.display = "block";
-      }
-      }
-    
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  let i;
+  let x = document.getElementsByClassName("character");
+  if (n > x.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = x.length };
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+    x[slideIndex - 1].style.display = "block";
+  }
+}
+
 
 
 
